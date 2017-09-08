ad5c7b141863e62bb5a3d9c343d3c2661312fe72
zsockboy2017z <zsockboy2012z@gmail.com>
2017-09-08 09:48:39 +0700

    commit

---
+++ app/src/main/java/mancoder/com/firstappgithub/Loginactivities.java
@@ -0,0 +1,10 @@
+package mancoder.com.firstappgithub;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ADMIN on 9/8/2017.
+ */
+
+public class Loginactivities extends AppCompatActivity {
+}
