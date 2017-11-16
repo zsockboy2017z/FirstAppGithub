a23d909afe3d7523453b0ba81609fbf70abedfb7
ManJeki <zsockboy2012z@gmail.com>
2017-11-15 19:45:18 -0800

    Them file CNTTK37a.java

---
+++ CNTTK37a.java
@@ -0,0 +1,23 @@
+/**
+ * @(#)CNTTK37a.java
+ *
+ *
+ * @author 
+ * @version 1.00 2017/11/15
+ */
+
+
+public class CNTTK37a {
+
+	private String hoTen;
+	private int namSinh;
+	
+	public CNTTK37a(String ht, int ns)
+	{
+		hoTen= ht;
+		namSinh= ns;
+	}
+    
+    
+    
+}
